e9f52efe5450db10dfadb14e9fbdee1904437a8f
Fernando Rodriguez Sela <frsela@tid.es>
2014-01-29 17:10:59 +0100

    Allow enable and disable a service. Useful for load balancers checks

--- routers/status.js
+++ routers/status.js
@@ -6,6 +6,9 @@
  * Fernando Rodríguez Sela <frsela@tid.es>
  */
 
+var log = require('../shared_libs/logger'),
+    querystring = require('querystring');
+
 module.exports.info = {
   name: 'statusRouter',
   type: 'router',
@@ -13,10 +16,40 @@ module.exports.info = {
   description: 'Used to check server status. Needed by load-balancers'
 };
 
+var server_status = {
+  available: true
+};
+
 module.exports.entrypoint = function router_status(parsedURL, body, req, res) {
-  // Return status mode to be used by load-balancers
-  res.setHeader('Content-Type', 'text/html');
-  res.statusCode = 200;
-  res.write('OK');
-  res.write('<br>common status to-be-done');
+  switch (req.method) {
+  case 'GET':
+    if (server_status.available === true) {
+      res.setHeader('Content-Type', 'text/plain');
+      res.statusCode = 200;
+      res.write('OK, server available !');
+    } else {
+      res.setHeader('Content-Type', 'text/plain');
+      res.statusCode = 400;
+      res.write('ERROR, server unavailable !');
+    }
+    break;
+  case 'POST':
+    var status_data = querystring.parse(body);
+    if (!status_data.enable) {
+      log.debug('WU_Router_exports --> No required data provided');
+      res.statusCode = 400;
+      res.write('Bad parameters. No required data provided' + JSON.stringify(status_data));
+      return;
+    }
+    server_status.available = (status_data.enable === 'true');
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 200;
+    break;
+  default:
+    response.setHeader('Content-Type', 'text/plain');
+    response.statusCode = 405;
+    response.write('Bad method. Only GET and POST is allowed');
+    log.debug('WU_Router_exports --> Bad method - ' +
+      req.method);
+  }
 };
