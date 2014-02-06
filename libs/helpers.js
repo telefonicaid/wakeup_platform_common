eb23af991538c0e115de13234edebc961156acf7
Fernando Rodríguez Sela <frsela@tid.es>
2014-02-06 08:23:25 +0100

    Update helpers.js

--- libs/helpers.js
+++ libs/helpers.js
@@ -18,11 +18,11 @@ module.exports.checkCallback = function checkCallback(callback) {
 
 function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+    .toString(16)
+    .substring(1);
 }
 
 module.exports.uuid = function guid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-      s4() + '-' + s4() + s4() + s4();
+    s4() + '-' + s4() + s4() + s4();
 };
