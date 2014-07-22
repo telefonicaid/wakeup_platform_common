59f4a0d48744e33f313b68df1031313b420690a6
Guillermo López Leal <willyaranda@gmail.com>
2014-07-22 13:09:39 +0200

    Fixing logger dates

--- libs/logger.js
+++ libs/logger.js
@@ -77,8 +77,8 @@ function logger() {
         var now = new Date();
         var logheader = '[' +
             now.getFullYear() + '-' +
-            String('00' + now.getMonth()).slice(-2) + '-' +
-            String('00' + now.getDay()).slice(-2) + ' ' +
+            String('00' + (now.getMonth() + 1)).slice(-2) + '-' +
+            String('00' + now.getDate()).slice(-2) + ' ' +
             String('00' + now.getHours()).slice(-2) + ':' +
             String('00' + now.getMinutes()).slice(-2) + ':' +
             String('00' + now.getSeconds()).slice(-2) + '.' +
